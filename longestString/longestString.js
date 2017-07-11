3c0589a141945d6a834e00a1c573d211253d5fd3
Kevin <anderkevin@gmail.comgit config --global user.name anderkevingit config --global user.email anderkevin@gmail.com>
2017-07-11 09:51:24 -0600

    partially completed needs more work

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -2,3 +2,11 @@
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
  */
+
+ const str = ([" "].split);
+ let longWord = 0;
+ for (let i = 0; i < str; i++) {
+   if (str[i].length > longWord){
+     longestWord = str[i].length;
+   };
+ }
